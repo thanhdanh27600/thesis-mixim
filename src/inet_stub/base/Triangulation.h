ec04eb9b2796af30b8f764fa199a69375d4494bb
ADMIN-PC\Admin <thanhdanh27600@gmail.com>
2022-01-17 19:15:44 +0700

    #add: init module Triangulation system which includes 3 circles

---
+++ src/inet_stub/base/Triangulation.h
@@ -0,0 +1,20 @@
+
+
+#include "FWMath.h"
+#include "Vector2D.h"
+#include "Line2D.h"
+#include "Coord.h"
+
+class Triangulation 
+{
+public:
+    /** @name Center1, Center2 and Center3 coordinates of the 3 circles. */
+    /*@{*/
+    Coord Center1;
+    Coord Center2;
+    Coord Center3;
+    /*@}*/
+
+private:
+
+};
